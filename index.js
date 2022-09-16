c8aa8f57976623b4403fdbf9b595085e4891d712
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-09-15 21:48:27 -0400

    commit tree 58215c1d62342987cdf815e9959110115c43d40e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const removeMain = document.querySelector('main');
+removeMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = '<h1>Jessica is the champion<h1>'; 
+
+
